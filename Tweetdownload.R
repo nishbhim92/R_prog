c65369d09855b089f98927be0cd31b8323949d65
Nishanth Bhimireddy <nishbhim92@gmail.com>
2021-04-09 11:09:48 +0100

    updated explanation

--- Tweetdownload.R
+++ Tweetdownload.R
@@ -10,16 +10,14 @@ access_key <- 'XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 access_keysecret <- 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 setup_twitter_oauth(api_key,api_keysecret,access_key,access_keysecret)
 
-#Get tweets
+#Get tweets of multiple users in this case I am taking tweets related to some ICOs
 boson <- userTimeline("BosonProtocol", n=100)
 apis <- userTimeline("_theAPIS_", n=100)
 converg <- userTimeline("ConvergenceFin", n=100)
 cookfin <- userTimeline("cook_finance", n=100)
 totem <- userTimeline("TotemFi", n=100)
 
-#write in to a csv
+#combine these tweets to form a data frame
 tweets<- tibble(map_df(c(boson,apis,converg,cookfin,totem),as.data.frame))  
+#write in to a csv
 write.csv(tweets, file="tweets.csv", row.names=FALSE)  
-
-#read data
-tweets<-read.csv("tweets.csv")
