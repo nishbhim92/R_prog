68a61b4092298c56e21c13bd4c56479ea9a9baa9
Nishanth Bhimireddy <nishbhim92@gmail.com>
2021-04-09 10:54:03 +0100

    clean code

--- Tweetdownload.R
+++ Tweetdownload.R
@@ -1,26 +1,25 @@
 #libraries
 library("twitteR")
-library("graphics")
-library("purrr")
-library("stringr")
-library("tm")
-library("syuzhet")
-library("dplyr")
+library("ROAuth")
+library("tidyverse")
 
 #connect to twitter
-api_key <- 'XXXX'
-api_keysecret <- 'XXXX'
-access_key <- 'XXXX'
-access_keysecret <- 'XXXX'
+api_key <- 'XXXXXXXXXXXXXXXXXXXXXX'
+api_keysecret <- 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_key <- 'XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_keysecret <- 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 setup_twitter_oauth(api_key,api_keysecret,access_key,access_keysecret)
+
 #Get tweets
 boson <- userTimeline("BosonProtocol", n=100)
 apis <- userTimeline("_theAPIS_", n=100)
 converg <- userTimeline("ConvergenceFin", n=100)
 cookfin <- userTimeline("cook_finance", n=100)
 totem <- userTimeline("TotemFi", n=100)
+
 #write in to a csv
-tweets<- tbl_df(map_df(c(boson,apis,converg,cookfin,totem),as.data.frame))  
+tweets<- tibble(map_df(c(boson,apis,converg,cookfin,totem),as.data.frame))  
 write.csv(tweets, file="tweets.csv", row.names=FALSE)  
-#readdata
+
+#read data
 tweets<-read.csv("tweets.csv")
