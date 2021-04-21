0f936dfd9a5bd10557826d0b43d3af881c811670
Nishanth Bhimireddy <nishbhim92@gmail.com>
2021-04-21 18:42:51 +0100

    v7

--- Redditapp/redditsentimentanalysis.R
+++ Redditapp/redditsentimentanalysis.R
@@ -21,7 +21,6 @@ library("igraph") #Network analysis library
 # https://igraph.org/r/
 library("tidyverse") # collection of some data science R packages https://www.tidyverse.org/
 
-
 #Extract required reddit links using this command in this case i am searching for "kingston" 
 # Number of pages to be searched for the given term is 2 in this case
 #Adjust number of pages based on your interest
